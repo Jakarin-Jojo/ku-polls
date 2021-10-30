3192563d55385aa9bfe640f9f8e94293973f62a8
Jakarin Chujan <jojogamer67@gmail.com>
2021-10-30 17:39:10 +0700

    Add Logging #44

--- polls/views.py
+++ polls/views.py
@@ -6,7 +6,9 @@ from django.views import generic
 from django.utils import timezone
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .models import Choice, Question
+from .models import Choice, Question, Vote
+import logging
+logger = logging.getLogger(__name__)
 
 
 class IndexView(generic.ListView):
@@ -52,9 +54,14 @@ def vote(request, question_id):
     Return: Render to a detail page if If no choice is selected.
     Else Redirect to a vote result page.
     """
+    user = request.user
+    logger.info("current user is", user.id, "login", user.username)
+    logger.info("Real name:", user.first_name, user.last_name)
     question = get_object_or_404(Question, pk=question_id)
+
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        logger.info(f'{user} voted on {question}')
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
@@ -62,8 +69,13 @@ def vote(request, question_id):
             'error_message': "You didn't select a choice.",
         })
     else:
-        selected_choice.votes += 1
-        selected_choice.save()
+        if question.vote_set.filter(user=user).exists():
+            vote = question.vote_set.get(user=user)
+            vote.choice = selected_choice
+            vote.save()
+        else:
+             Vote.objects.create(user=user, choice=selected_choice)
+        logger.info(f"User {user.username} submit a vote for question {question.id} ")
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
